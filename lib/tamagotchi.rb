a8670d78306ffc32778c2fe48187bca9c581d636
Colin W Kingen and Sam Park <colin@>
2016-07-06 10:52:25 -0700

    add functions to check and modify attributes

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,17 +1,68 @@
 class Tamagotchi
   define_method(:initialize) do |name|
     @name = name
+    @food_level = 10
+    @sleep_level = 10
+    @activity_level = 10
   end
   define_method(:food_level) do
-    @food_level = 10
+    @food_level
   end
   define_method(:sleep_level) do
-    @sleep_level = 10
+    @sleep_level
   end
   define_method(:activity_level) do
-    @activity_level = 10
+    @activity_level
   end
   define_method(:name) do
     @name
+  end
+  define_method(:is_alive) do
+    if self.food_level > 0 && self.sleep_level > 0
+      return true
+    else
+      return false
+    end
+  end
+  define_method(:is_tired) do
+    if self.sleep_level < 5
+      return true
+    else
+      return false
+    end
+  end
+  define_method(:is_hungry) do
+    if self.food_level < 5
+      return true
+    else
+      return false
+    end
+  end
+  define_method(:is_bored) do
+    if self.activity_level < 5
+      return true
+    else
+      return false
+    end
+  end
+  define_method(:feed) do
+    if self.food_level < 10
+      @food_level += 3
+    end
+  end
+  define_method(:play) do
+    if self.activity_level < 10
+      @activity_level += 2
+    end
+  end
+  define_method(:to_bed) do
+    if self.sleep_level < 10
+      @sleep_level += 4
+    end
+  end
+  define_method(:change_attr) do |food, sleep_level, activity_level|
+    @sleep_level = sleep_level
+    @food_level = food
+    @activity_level = activity_level
   end
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -4,11 +4,70 @@ require('tamagotchi')
 describe(Tamagotchi) do
   describe("#initialize") do
     it("sets the name and life levels of a new tamagotchi") do
-      my_pet = Tamagotchi.new("lil dragon")
-      expect(my_pet.name()).to(eq("lil dragon"))
+      my_pet = Tamagotchi.new("barry")
+      expect(my_pet.name()).to(eq("barry"))
       expect(my_pet.sleep_level()).to(eq(10))
       expect(my_pet.food_level()).to(eq(10))
       expect(my_pet.activity_level()).to(eq(10))
+    end
+  end
+  describe("#is_alive") do
+    it("tells you if tamagotchi is alive") do
+      my_pet = Tamagotchi.new("barry")
+      expect(my_pet.is_alive()).to(eq(true))
+      my_pet.change_attr(0,0,0)
+      expect(my_pet.is_alive()).to(eq(false))
+    end
+  end
+  describe("#is_tired") do
+    it("tells you if tamagotchi is tired") do
+      my_pet = Tamagotchi.new("barry")
+      my_pet.change_attr(10,10,10)
+      expect(my_pet.is_tired()).to(eq(false))
+      my_pet.change_attr(10,3,10)
+      expect(my_pet.is_tired()).to(eq(true))
+    end
+  end
+  describe("#is_hungry") do
+    it("tells you if tamagotchi is hungry") do
+      my_pet = Tamagotchi.new("barry")
+      my_pet.change_attr(10,10,10)
+      expect(my_pet.is_hungry()).to(eq(false))
+      my_pet.change_attr(3,10,10)
+      expect(my_pet.is_hungry()).to(eq(true))
+    end
+  end
+  describe("#is_bored") do
+    it("tells you if your tamagotchi is bored") do
+      my_pet = Tamagotchi.new("barry")
+      my_pet.change_attr(10,10,10)
+      expect(my_pet.is_bored()).to(eq(false))
+      my_pet.change_attr(3,10,3)
+      expect(my_pet.is_bored()).to(eq(true))
+    end
+  end
+  describe("#feed") do
+    it("allows you to feed your tamagotchi") do
+      my_pet = Tamagotchi.new("barry")
+      my_pet.change_attr(5,10,10)
+      my_pet.feed()
+      expect(my_pet.food_level()).to(eq(8))
+    end
+  end
+  describe("#to_bed") do
+    it("allows your tamagotchi to sleep") do
+      my_pet = Tamagotchi.new("barry")
+      my_pet.change_attr(10,4,10)
+      my_pet.to_bed()
+      expect(my_pet.sleep_level()).to(eq(8))
+    end
+  end
+  describe("#play") do
+    it("allows your tamagotchi to play") do
+      my_pet = Tamagotchi.new("barry")
+      my_pet.change_attr(10,10,4)
+      my_pet.play()
+      expect(my_pet.activity_level()).to(eq(6))
     end
   end
 end
